e7534a45db2def9f4dddba7e1cb6b01c0dfef2d7
Josip Medved <jmedved@jmedved.com>
2023-06-28 16:11:58 -0700

    Added microphone state handling

--- src/WyzePlugControl/App.cs
+++ src/WyzePlugControl/App.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using WyzeApi;
 
 namespace WyzePlugControl {
@@ -8,27 +10,25 @@ namespace WyzePlugControl {
             Medo.Application.SingleInstance.Attach();  // will exit on its own
 
             var plugId = (args.Length >= 1) ? args[0] : "";
-            var desiredState = (args.Length >= 2) ? args[1] : "";
 
             var email = Environment.GetEnvironmentVariable("WYZE_EMAIL") ?? "";
             var password = Environment.GetEnvironmentVariable("WYZE_PASSWORD") ?? "";
             var totp = Environment.GetEnvironmentVariable("WYZE_TOTP") ?? "";
-            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password)){
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password)) {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Must define WYZE_EMAIL and WYZE_PASSWORD environment variables.");
                 Console.ResetColor();
                 Environment.Exit(1);
             }
 
+            WyzePlugDevice? selectedPlug = null;
             using var wyze = new Wyze();
             if (wyze.Login(email, password, totp)) {
                 foreach (var device in wyze.GetDevices()) {
                     if (device is WyzePlugDevice plug) {
-                        if (plug.Id == plugId) {
+                        if (plug.Id.Equals(plugId, StringComparison.OrdinalIgnoreCase)) {
                             Console.ForegroundColor = ConsoleColor.Green;
-                            if (bool.TryParse(desiredState, out var newState)) {
-                                plug.SetPowerState(newState);
-                            }
+                            selectedPlug = plug;
                         } else {
                             Console.ForegroundColor = ConsoleColor.Yellow;
                         }
@@ -45,6 +45,54 @@ namespace WyzePlugControl {
                 Console.ResetColor();
                 Environment.Exit(2);
             }
+
+            if (selectedPlug == null) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Cannot find plug.");
+                Console.ResetColor();
+                Environment.Exit(2);
+            }
+
+
+            Thread.Sleep(1000);
+
+            var internalMicStartInfo = new ProcessStartInfo {
+                FileName = @"/usr/bin/amixer",
+                Arguments = "get Capture",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            bool? lastState = null;
+            while (true) {
+                var result = Process.Start(internalMicStartInfo)!;
+                if (result != null) {
+                    var outputText = result.StandardOutput.ReadToEnd();
+                    var nextState = !Grep(outputText, "[off]");
+
+                    if (DateTime.Now.Hour is < 8 or >= 22) { nextState = false; }  // force disable between 22:00 and 08:00
+
+                    if (nextState != lastState) {
+                        selectedPlug.SetPowerState(nextState);
+                        lastState = nextState;
+                    }
+                } else {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("No output");
+                    Console.ResetColor();
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+
+        private static bool Grep(string text, string filter) {
+            var lines = text.Split("\n");
+            foreach (var line in lines) {
+                if (line.Contains(filter)) { return true; }
+            }
+            return false;
         }
 
     }
